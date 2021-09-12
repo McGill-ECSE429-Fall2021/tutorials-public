7de81671a4598f69b6fffed72dde0cb78ef28857
SogolMasoumzadeh <sogol.masoumzadeh@mail.mcgill.ca>
2021-09-12 13:56:04 -0400

    Adding the sample project for tutorial 2

---
+++ Documents/Projects/TA/Fall 2021/tutorial2/Tutorial.2/src/main/java/McGill/ECSE429/App.java
@@ -0,0 +1,13 @@
+package McGill.ECSE429;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
